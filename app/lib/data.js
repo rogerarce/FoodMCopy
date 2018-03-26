0fe842d81ca9d27b60907b81f62640a129ab2e00
Sean Arce <arceseanroger@gmail.com>
2018-03-26 22:52:09 +0800

    scenes basic feature

---
+++ app/lib/data.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    id: 1,
+    name: 'Ensaimada',
+    description: 'Elit vero perferendis maiores eius soluta minima dolores consequatur repellat?',
+    price: '5',
+    img: "https://fthmb.tqn.com/dbVPfB7jqySuceemcskk3lWP_z8=/960x0/filters:no_upscale()/Ensaimada-5a70ca746edd65003661b1d5.jpg",
+    prep_time: '5 mins',
+  },
+  {
+    id: 2,
+    name: 'Pandesal',
+    description: 'Elit vero perferendis maiores eius soluta minima dolores consequatur repellat?',
+    price: '5',
+    img: "https://i2.wp.com/www.onegreenplanet.org/wp-content/uploads/2017/03/image1-20.jpg?fit=1200%2C750&ssl=1",
+    prep_time: '5 mins',
+  },
+  {
+    id: 3,
+    name: 'Brazo de mercedez',
+    description: 'Elit vero perferendis maiores eius soluta minima dolores consequatur repellat?',
+    price: '15',
+    img: "https://www.kawalingpinoy.com/wp-content/uploads/2014/07/brasodemercedes1.jpg",
+    prep_time: '5 mins',
+  }
+]
+
--- app/screens/Dashboard.js
+++ app/screens/Dashboard.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, Image } from 'react-native'
+import data from '../lib/data'
 
 export default class Dashboard extends Component {
+
+  _keyExtractor = (item, index) => item.name
+
+  productItem ({ item }) {
+    return (
+      <View>
+        <Image style={{ width: 150, height: 150 }} source={{ uri: item.img }} />
+        <Text>{item.name}</Text>
+        <Text>{item.description}</Text>
+        <Text>{item.price}</Text>
+        <Text>{item.prep_time}</Text>
+      </View>
+    )
+  }
+  
   render() {
     return (
       <View>
-        <Text>Dashboard</Text>
+        <Text>Dashboard Display</Text>
+        <FlatList 
+          data={data} 
+          keyExtractor={this._keyExtractor}
+          renderItem={this.productItem}
+        />
       </View>
     )
   }
