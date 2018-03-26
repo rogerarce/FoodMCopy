558fc9d60a05975e4955079f689e922c9894c233
Sean Roger <sean.arce@intechsive.com>
2018-03-26 21:29:50 +0800

    Initial Commit

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text, FlatList } from 'react-native'
+
+export default class Dashboard extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Dashboard</Text>
+      </View>
+    )
+  }
+}
